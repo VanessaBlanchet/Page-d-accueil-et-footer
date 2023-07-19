a42a12d20b714768b907039e7fb9fa8ced08a2c2
VanessaBlanchet <miminemail@gmail.com>
2023-07-19 11:55:21 +0200

    révision JS

---
+++ Scripts/main.js
@@ -0,0 +1,17 @@
+let monAge = 39 
+const monPrenom = "Vanessa"
+
+
+
+
+
+let messageBienvenue = "Bonjour, je m'appelle " + monPrenom + ", j'ai " + monAge + " ans. "
+
+let messagePerso = messageBienvenue + monPrenom
+
+
+
+console.log(messageBienvenue)
+
+
+// Prochain cours : "https://openclassrooms.com/fr/courses/7696886-apprenez-a-programmer-avec-javascript/8204834-structurez-des-donnees-grace-aux-objets"
